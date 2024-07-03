24900cb1efc56390bd7f0e955379fd3bfb2f0cf4
Brad Nguyen <bradcodecraft@gmail.com>
2024-07-03 12:29:50 -0400

    connect mongoDB and create Schema

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI)
+
+// Schemas
+const User = new mongoose.Schema({
+  "username": String,
+  "_id": String,
+})
+const Exercise = new mongoose.Schema({
+  "username": String,
+  "description": String,
+  "duration": Number,
+  "date": String,
+  "_id": String
+})
+const Log = new mongoose.Schema({
+  "username": String,
+  "count": Number,
+  "_id": String,
+  "log": [{
+    "description": String,
+    "duration": Number,
+    "date": String,
+  }]
+})
 
 app.use(cors())
 app.use(express.static('public'))
